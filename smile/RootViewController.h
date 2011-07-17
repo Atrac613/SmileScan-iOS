c7ff9e799b8fee69bb73fb7ed08f4b55da30578b
Osamu Noguchi <atrac613@gmail.com>
2011-07-17 22:38:53 +0900

    first commit

--- smile/RootViewController.h
+++ smile/RootViewController.h
@@ -7,10 +7,37 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <opencv2/imgproc/imgproc_c.h>
+#import <opencv2/objdetect/objdetect.hpp>
 
-@interface RootViewController : UITableViewController {
-
+@interface RootViewController : UIViewController <AVCaptureVideoDataOutputSampleBufferDelegate> {
+    AVCaptureSession *captureSession;
+    AVCaptureVideoDataOutput *videoOutput;
+    
+    CvHaarClassifierCascade *cascade;
+    CvMemStorage *storage;
+    
+    NSArray *smileRectangles;
+    
+    UIImageView *smileView;
 }
 
+@property (nonatomic, retain) AVCaptureSession *captureSession;
+@property (nonatomic, retain) AVCaptureVideoDataOutput *videoOutput;
+
+@property (nonatomic, retain) NSArray *smileRectangles;
+
+@property (nonatomic, retain) UIImageView *smileView;
+
+- (AVCaptureDevice *)frontFacingCameraIfAvailable;
+
+- (void)initOpenCV;
+- (void)releaseOpenCV;
+
+- (void)opencvSmileDetect:(UIImage *)originalImage;
+
+- (UIImage *)imageFromSampleBuffer:(CMSampleBufferRef)sampleBuffer;
+- (IplImage *)rotateImage:(IplImage *)img angle:(double)angle;
 
 @end
